13783c491bca2ad2d5aee4c08a03e2556d7f9015
Benjamin Goering <bengoering@gmail.com>
2014-04-07 20:38:51 -0700

    Add demo page

--- demos/main/main-demo.js
+++ demos/main/main-demo.js
@@ -1,3 +1,9 @@
 var Livefyre = require('Livefyre');
+var livefyreAuthDelegate = require('auth-livefyre/livefyre-auth-delegate');
+var createAuthButton = require('auth/contrib/auth-button');
+
+Livefyre.auth.delegate(livefyreAuthDelegate('http://livefyre.com'));
+
+createAuthButton(Livefyre.auth, document.getElementById('auth-button'));
 
 console.log(Livefyre);
