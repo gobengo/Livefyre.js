462acb12838a06612d6b64d6f58b2645e6deba61
Benjamin Goering <bengoering@gmail.com>
2014-05-21 22:36:03 -0700

    Remove unused thing in main

--- src/main.js
+++ src/main.js
@@ -3,11 +3,6 @@ var lfRequire = require('livefyre-require');
 
 // Exports .require, .define, .requirejs
 exports = module.exports = lfRequire;
-
 exports['livefyre-auth'] = auth;
-
-lfRequire.define('auth', function () {
-    return auth;
-});
 
 // maybe: Add other Livefyre.things on exports
