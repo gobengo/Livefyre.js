b56f9deb14e6dd399bf7be84f749becdb3e9ddc6
JJ Weber <jjweber@gmail.com>
2014-07-14 11:52:28 -0700

    Fix typo

--- config/requirejs.conf.js
+++ config/requirejs.conf.js
@@ -10,7 +10,7 @@ require.config({
     chai: 'node_modules/chai/chai',
     debug: 'lib/debug/debug',
     requireLib: 'lib/requirejs/require',
-    semver: 'lib/node-lite/semver.lite',
+    semver: 'lib/semver-lite/semver.lite',
     'text': 'lib/requirejs-plugins/lib/text',
     purl: 'lib/purl/purl'
   },
